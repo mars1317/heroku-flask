8a38abb912988ef0e0f09fdbddda40cdab02166f
servatmandmariam1 <37312219+servatmandmariam1@users.noreply.github.com>
2019-11-25 19:15:08 +0200

    Add files via upload

--- dao/data.py
+++ dao/data.py
@@ -1,5 +1,5 @@
 host = '127.0.0.1'
-database = 'receipt_webservice'
+database = 'dish_service'
 username = 'postgres'
 password = '134951m'
 DATABASE_URL = 'postgresql://postgres:134951m@localhost/dish_service'
--- dao/orm/entities.py
+++ dao/orm/entities.py
@@ -6,26 +6,25 @@ from dao.orm.entities import *
 Base = declarative_base()
 class Dish(Base):
     __tablename__ = 'dish'
-    dishname = Column(String(80), primary_key = True)
+    dishname = Column(String(80), primary_key=True)
     calories_amount = Column(Integer, nullable=False)
 
 class Type(Base):
-    __tablename__ = "type"
-    typename = Column(String(80), primary_key= True)
+    __tablename__ = 'type'
+    id = Column(Integer, primary_key=True)
+    typename = Column(String(80))
+    dishname_fk = Column(String(100), ForeignKey('dish.dishname'))
+
 
 class Receipt(Base):
     __tablename__ = 'receipt'
-    dishname_fk = Column(Integer, ForeignKey('dish.dishname'), primary_key=True)
-    receipt_content = Column (String(100), primary_key = True)
-    def __init__(self, receipt_content, dishname_fk):
-        self.dishname_fk = dishname_fk
-        self.receipt_content = receipt_content
+    dishname_fk = Column(String(100), ForeignKey('dish.dishname'))
+    receipt_content = Column(String(100), primary_key=True)
+
+
 class Ingridients(Base):
     __tablename__ = 'ingridient'
-
-    ingridientname = Column(String(80), primary_key = True)
-    def __init__(self, ingridientname):
-        self.ingridientname = ingridientname
+    ingridientname = Column(String(80), primary_key=True)
 
 
 class Dish_Type(Base):
@@ -33,3 +32,16 @@ class Dish_Type(Base):
     dishname = Column(String(255), ForeignKey('Dish.dishname'), primary_key=True)
     type = Column(String(255), primary_key=True)
 
+class Restaurant(Base):
+    __tablename__ = 'restaurant'
+    address = Column(String(80))
+    city = Column(String(80))
+    star = Column(Integer, nullable=False)
+    country = Column(String(80))
+    name = Column(String, primary_key=True)
+    dishname_fk = Column(String(80))
+
+
+
+
+    dishname_fk = Column(String(100), ForeignKey('dish.dishname'))
