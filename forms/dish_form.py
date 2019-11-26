e8814483a6ac5f6cb180e3a7377ad5316f8014a3
servatmandmariam1 <37312219+servatmandmariam1@users.noreply.github.com>
2019-11-26 17:34:42 +0200

    Update dish_form.py

--- forms/dish_form.py
+++ forms/dish_form.py
@@ -1,18 +1,72 @@
 from flask_wtf import Form
-from wtforms import StringField, SubmitField, HiddenField
+from wtforms import StringField, IntegerField, SubmitField, HiddenField
 from wtforms import validators
 
 
 class DishForm(Form):
-    dishname = StringField("Name: ", [
-        validators.DataRequired("Please enter discipline name."),
-        validators.Length(3, 200, "Name should be from 3 to 200 symbols")
-    ])
-    dishtype = StringField("Teacher Name: ", [
-        validators.DataRequired("Please enter teacher name."),
-        validators.Length(3, 255, "Name should be from 3 to 255 symbols")
-    ])
-
+    dishname = StringField("Dish name: ", [
+        validators.DataRequired("Please enter dish name."),
+        validators.Length(3, 100, "Name should be from 3 to 100 symbols")])
+    calories_amount = IntegerField("Group: ", [
+        validators.DataRequired("Please enter calories amount")])
+    calories_amount = IntegerField("Calories amount: ", [
+        validators.DataRequired("Please enter calories amount"), validators.NumberRange(min=1, max=5000)])
     old_name = HiddenField()
 
     submit = SubmitField("Save")
+class ReceiptForm(Form):
+    __tablename__ = 'receipt'
+    dishname_fk = StringField("Dish name: ", [
+        validators.DataRequired("Please enter dish name."),
+        validators.Length(3, 255, "Name should be from 3 to 255 symbols")])
+    receipt_content = StringField("Dish Receipt: ", [
+        validators.DataRequired("Please enter dish Receipt."),
+        validators.Length(3, 255, "Receipt should be from 3 to 255 symbols")])
+    old_name = HiddenField()
+
+    submit = SubmitField("Save")
+class TypeForm(Form):
+    typename = StringField("Type name: ", [
+        validators.DataRequired("Please enter type name."),
+        validators.Length(3, 255, "Name should be from 3 to 255 symbols")])
+    dishname_fk = StringField("Dish name: ", [
+        validators.DataRequired("Please enter dish name."),
+        validators.Length(3, 255, "Name should be from 3 to 255 symbols")])
+    old_name = HiddenField()
+    old_id = IntegerField(HiddenField())
+    submit = SubmitField("Save")
+class RestorauntForm(Form):
+    name = StringField("Name of restoraunt: ", [
+        validators.DataRequired("Please enter name of restoraunt."),
+        validators.Length(3, 255, "Name should be from 3 to 255 symbols")])
+    dishname_fk = StringField("Dish name: ", [
+        validators.DataRequired("Please enter dish name."),
+        validators.Length(3, 255, "Name should be from 3 to 255 symbols")])
+    star = IntegerField("Star of restoraunt: ", [
+        validators.DataRequired("Please enter star of restoraunt."),
+        validators.NumberRange(min=1, max=5)]
+                        )
+
+    country = StringField("country of restoraunt: ", [
+        validators.DataRequired("Please enter country of restoraunt."),
+        validators.Length(3, 255, "country should be from 3 to 255 symbols")])
+    city = StringField("city of restoraunt: ", [
+        validators.DataRequired("Please enter city of restoraunt."),
+        validators.Length(3, 20, "city should be from 3 to 255 symbols")])
+    address = StringField("adress of restoraunt: ", [
+        validators.DataRequired("Please enter adress of restoraunt."),
+        validators.Length(3, 20, "adress should be from 3 to 255 symbols")])
+    old_name = HiddenField()
+    submit = SubmitField("Save")
+
+
+# class IngridientForm(Form):
+#     dishname = StringField("Dish name: ", [
+#         validators.DataRequired("Please enter dish name."),
+#         validators.Length(3, 255, "Name should be from 3 to 255 symbols")])
+#     ingridients = StringField("Ingridients: ", [
+#         validators.DataRequired("Please enter dish ingridients."),
+#         validators.Length(3, 255, "Ingridient should be from 3 to 255 symbols")])
+#     old_name = HiddenField()
+#
+#     submit = SubmitField("Save")
